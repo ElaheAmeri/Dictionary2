348d9ef15b8ed9ce410e3e9d1419b102adc935a5
Elahe Ameri <elahelastname@gmail.com>
2022-05-04 11:58:09 -0700

    Repository was created

---
+++ app/src/main/java/com/example/dictionary2/dataBase/DictionaryRepository.kt
@@ -0,0 +1,37 @@
+package com.example.dictionary2.dataBase
+
+import android.content.Context
+
+object DictionaryRepository {
+    lateinit var db: AppDataBase
+    lateinit var dao: DictionaryDao
+    var wordList= listOf<Word>(
+        Word( 1,"Hello","سلام","Hello My friend","Hi"),
+        Word(2,"Success","موفقیت","Success awaits you","Smash"),
+        Word(3,"Dream","رویا","Don't Let Your Dream Be Dream","Growing")
+    )
+    fun initDB(context: Context){
+        db = AppDataBase.getAppDatabase(context)
+        dao = db.DictionaryDao()
+    }
+
+
+    fun getWord(ENG:String): Word?{
+        return dao?.getWord(ENG)
+    }
+    fun deletWord(word: Word){
+        dao?.delet(word)
+    }
+    fun updateTable(word: Word){
+        dao?.update(word)
+    }
+    fun getAllWord():List<Word>?{
+        return dao?.getAllWord()
+    }
+    fun insert(word: Word){
+        dao?.insert(word)
+    }
+    fun insertListOfWord(words:List<Word>){
+        dao?.insertAll(words)
+    }
+}
